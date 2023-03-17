f4ed2ce3da32fd185e853cf5877fd6a8f9287138
danielecaporaletti <danielecaporaletti1@gmail.com>
2023-03-17 15:52:28 +0100

    redux

---
+++ Start2Impact/REACT/03_redux/src/actions/index.js
@@ -0,0 +1,11 @@
+export const increment = () => {
+    return {
+        type: 'INCREMENT',
+    }
+}
+export const decrement = (num) => {
+    return {
+        type: 'DECREMENT',
+        payload: num,
+    }
+}
