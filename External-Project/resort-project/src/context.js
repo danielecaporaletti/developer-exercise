36f1b4a2c5d6e16c096541d05c4f242dcc93864c
danielecaporaletti <danielecaporaletti1@gmail.com>
2023-03-22 22:42:56 +0100

    react

---
+++ External-Project/resort-project/src/context.js
@@ -0,0 +1,44 @@
+import React, { useEffect } from 'react';
+import { useState } from 'react';
+import items from './data';
+
+export const RoomContext = React.createContext();
+
+const RoomProvider = ({ children }) => {
+  const [state, setState] = useState({
+    rooms: [],
+    sortedRooms: [],
+    featuredRooms: [],
+    loading: false,
+  });
+
+  useEffect(() => {
+    const formatRooms = formatData(items);
+    setState(prevState => ({ ...prevState, rooms: formatRooms }));
+    setState(prevState => ({ ...prevState, featuredRooms: prevState.rooms.filter(room => room.featured === true) }));
+    setState(prevState => ({ ...prevState, sortedRooms: formatRooms }));
+    //setState((prevState) => ({ ...prevState, loading: false }));
+  }, []);
+
+  const formatData = (items) => {
+    let tempItems = items.map((item) => {
+      let id = item.sys.id;
+      let images = item.fields.images.map((image) => {
+        return image.fields.file.url;
+      });
+      let room = { ...item.fields, images, id };
+      return room;
+    });
+    return tempItems;
+  };
+
+  return (
+    <>
+      <RoomContext.Provider value={{ ...state }}>
+        {children}
+      </RoomContext.Provider>
+    </>
+  );
+};
+
+export default RoomProvider;
